7ce392a1a27be0f6f73776a96854688b9077891f
Nathan Hoche <hochenat@gmail.com>
2023-12-03 21:14:25 +0100

    [ADD]: V1

---
+++ README.md
@@ -0,0 +1,11 @@
+# SLN Group Project
+
+# Exercise
+
+| Number | Name | Done |
+|--------|------|------|
+| 1 | data distribution and the law of large numbers | ✅ |
+| 2 | meteorological data : dimensionality reduction and visualization | ✅ |
+| 3 | company clustering customers | ✅ |
+| 4 | exploitation/exploration compromise | ✅ |
+| 5 | application of unsupervised learning | ✅ |
---
+++ exercise_4/sln_policy.py
@@ -0,0 +1,23 @@
+from agent import Agent
+
+
+CurrentMove = "left"
+
+def sln_policy(agent: Agent) -> str:
+    """
+    Policy of the agent
+    return "left", "right", or "none"
+    """
+    global CurrentMove
+
+    # Move the agent if there is no known reward or if the current score is less than 20
+    if (agent.known_rewards.sum() <= 20):
+        # If the agent is in the first postion, move right
+        if (agent.position == 0):
+            CurrentMove = "right"
+        # If the agent is in the last postion, move left
+        elif (agent.position == 7):
+            CurrentMove = "left"
+        return CurrentMove
+    # Stay in the reward position if the agent knows the reward
+    return "none"
